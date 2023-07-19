680dcddf9ea898d9d75e11aa97206fac671cb989
SudhaCheemala <csudharani37@gmail.com>
2023-07-19 12:08:38 +0530

    day-21

---
+++ Day-21/list_tuple_slice_python.py
@@ -0,0 +1,6 @@
+# -------------------------How to slice the LIST AND TUPLES in python-------------------------
+piano_keys=["a","b","c","d","e","f","g","h","i","j"]
+piano_keys_tuple=("ae","br","cr","d4","e3","3f","4g","2h","1i","4j")
+# reverse the list
+print(piano_keys[::-1])
+print(piano_keys_tuple[2:6])
